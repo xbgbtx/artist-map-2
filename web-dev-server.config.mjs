dcd63452cc25a4b4a5491f7cba3b1016924226a8
Virthe <virthe@accursed-lands.net>
2022-07-05 13:52:36 +0100

    Add rollup replace to dev-server

--- web-dev-server.config.mjs
+++ web-dev-server.config.mjs
@@ -1,5 +1,10 @@
 // import { hmrPlugin, presets } from '@open-wc/dev-server-hmr';
 
+import { fromRollup } from '@web/dev-server-rollup';
+import rollupReplace from '@rollup/plugin-replace';
+
+const replace = fromRollup(rollupReplace);
+
 /** Use Hot Module replacement by adding --hmr to the start command */
 const hmr = process.argv.includes('--hmr');
 
@@ -10,7 +15,7 @@ export default /** @type {import('@web/dev-server').DevServerConfig} */ ({
   nodeResolve: {
     exportConditions: ['browser', 'development'],
   },
-  
+
   /** Compile JS for older browsers. Requires @web/dev-server-esbuild plugin */
   // esbuildTarget: 'auto'
 
@@ -18,8 +23,10 @@ export default /** @type {import('@web/dev-server').DevServerConfig} */ ({
   // appIndex: 'demo/index.html',
 
   plugins: [
-    /** Use Hot Module Replacement by uncommenting. Requires @open-wc/dev-server-hmr plugin */
-    // hmr && hmrPlugin({ exclude: ['**/*/node_modules/**/*'], presets: [presets.litElement] }),
+    /** Fix for xstate */
+    replace({
+      'process.env.NODE_ENV': process.env.NODE_ENV,
+    }),
   ],
 
   // See documentation for all available options
