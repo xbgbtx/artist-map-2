88d367d1ba77329a4f7c22686286db31316c147a
Virthe <virthe@accursed-lands.net>
2022-07-05 13:10:16 +0100

    xstate machine creation

--- src/artist-map-2.ts
+++ src/artist-map-2.ts
@@ -1,3 +1,11 @@
+import { createMachine } from 'xstate';
 import { ArtistMap2 } from './ArtistMap2.js';
+
+const response = await fetch('xstate/ArtistMap.json');
+const xstateData = await response.json();
+
+const appMachine = createMachine(xstateData);
+
+console.log(appMachine);
 
 customElements.define('artist-map-2', ArtistMap2);
