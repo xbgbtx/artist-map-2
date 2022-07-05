62270a316a6c1f5343c1dae25f8f8ea5e027876d
Virthe <virthe@accursed-lands.net>
2022-07-05 16:23:13 +0100

    npm install rollup-plugin-copy -D

--- src/artist-map-2.ts
+++ src/artist-map-2.ts
@@ -1,6 +1,10 @@
 /* import { createMachine } from 'xstate'; */
 import { ArtistMap2 } from './ArtistMap2.js';
 
+import * as json from '../xstate/ArtistMap.json';
+
+console.log(json);
+
 /* const response = await fetch('xstate/ArtistMap.json'); */
 /* const xstateData = await response.json(); */
 
