71de6afa98dd97748faa21ef435bde3345337674
Virthe <virthe@accursed-lands.net>
2022-07-08 13:07:37 +0100

    Import leaflet

--- src/logic/Leaflet.ts
+++ src/logic/Leaflet.ts
@@ -1,33 +1,32 @@
-import * as L from 'leaflet'
-import { dispatchAppEvent } from './AppEvents.js'
-import { ArtistMapContext } from '../ArtistMapTypes.js'
+import 'leaflet';
+import { dispatchAppEvent } from './AppEvents.js';
+import { ArtistMapContext } from '../ArtistMapTypes.js';
 
-const sleep = (milliseconds: number) => 
-    new Promise(resolve => setTimeout(resolve, milliseconds));
+const sleep = (milliseconds: number) =>
+  new Promise(resolve => setTimeout(resolve, milliseconds));
 
-const attributionStr = 
-    "&copy; <a href=\"https://openstreetmap.org/copyright\">"         +
-                        "OpenStreetMap contributors</a> | "           +
-    "<a href=\"https://www.wikidata.org\" >Wikidata</a> | "           +
-    "<a href=\"https://github.com/xbgbtx/ArtistMap\" >Source Code</a>";
+const {L} = window;
 
-export function createMap () {
-  console.log(L);
-    const map = L.map ("mapdiv");
+const attributionStr =
+  '&copy; <a href="https://openstreetmap.org/copyright">' +
+  'OpenStreetMap contributors</a> | ' +
+  '<a href="https://www.wikidata.org" >Wikidata</a> | ' +
+  '<a href="https://github.com/xbgbtx/ArtistMap" >Source Code</a>';
 
-    // Add OpenStreetMap tiles
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", 
-    {
-        maxZoom: 19,
-        attribution: attributionStr,
-        noWrap: false
-    }).addTo(map);
+export function createMap() {
+  const map = L.map('mapdiv');
+
+  // Add OpenStreetMap tiles
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: attributionStr,
+    noWrap: false,
+  }).addTo(map);
 }
 
-export async function buildMap ( ctx: ArtistMapContext ) {
+export async function buildMap(ctx: ArtistMapContext) {
   await sleep(1000);
-  setTimeout(()=>{
+  setTimeout(() => {
     dispatchAppEvent({ type: 'MapBuildComplete' });
-  },2000);
+  }, 2000);
 }
-
